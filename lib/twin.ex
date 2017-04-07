9317d5d7b46b22df3a56c1b12278d40055433599
Tymon Tobolski <i@teamon.eu>
2017-04-07 22:45:31 +0200

    init

---
+++ lib/twin.ex
@@ -0,0 +1,18 @@
+defmodule Twin do
+  @moduledoc """
+  Documentation for Twin.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Twin.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/twin_test.exs
@@ -0,0 +1,8 @@
+defmodule TwinTest do
+  use ExUnit.Case
+  doctest Twin
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
